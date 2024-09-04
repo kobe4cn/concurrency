984dd3da9b2040a9dbdc0d233be1b67140dbfa2a
kobe4cn <kevin.yang.xgz@gmail.com>
2024-09-04 21:41:11 +0800

    thread for matrix dot product

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
 mod matrix;
+mod vector;
 pub use matrix::multiply;
 pub use matrix::Matrix;
+pub use vector::{dot_product, Vector};
--- src/matrix.rs
+++ src/matrix.rs
@@ -1,8 +1,34 @@
+use std::{sync::mpsc, thread};
+
+use crate::{dot_product, Vector};
+
 #[derive(Debug)]
 pub struct Matrix<T> {
     data: Vec<T>,
     rows: usize,
     cols: usize,
+}
+
+const NUM_THREADS: usize = 4;
+
+pub struct MsgInput<T> {
+    idx: usize,
+    row: Vector<T>,
+    col: Vector<T>,
+}
+
+pub struct MsgOutput<T> {
+    idx: usize,
+    value: T,
+}
+pub struct Msg<T> {
+    input: MsgInput<T>,
+    sender: oneshot::Sender<MsgOutput<T>>,
+}
+impl<T> Msg<T> {
+    fn new(input: MsgInput<T>, sender: oneshot::Sender<MsgOutput<T>>) -> Self {
+        Self { input, sender }
+    }
 }
 
 pub fn multiply<T>(a: &Matrix<T>, b: &Matrix<T>) -> anyhow::Result<Matrix<T>>
@@ -11,6 +37,8 @@ where
         + std::ops::AddAssign
         + Copy
         + Default
+        + Send
+        + 'static
         + std::fmt::Debug
         + std::fmt::Display
         + std::ops::Add<Output = T>,
@@ -19,14 +47,56 @@ where
         anyhow::bail!("Incompatible matrix dimensions");
     }
 
-    let mut data = vec![T::default(); a.rows * b.cols];
+    let senders = (0..NUM_THREADS)
+        .map(|_| {
+            let (tx, rx) = mpsc::channel::<Msg<T>>();
+            thread::spawn(move || {
+                for msg in rx {
+                    let value = dot_product(msg.input.row, msg.input.col)?;
+                    if let Err(e) = msg.sender.send(MsgOutput {
+                        idx: msg.input.idx,
+                        value,
+                    }) {
+                        eprintln!("Error: {:?}", e);
+                    };
+                }
+                Ok::<(), anyhow::Error>(())
+            });
+            tx
+        })
+        .collect::<Vec<_>>();
+    //generate 4 threads which will receive the msg and do dot product
+
+    let matrix_len = a.rows * b.cols;
+    let mut data = vec![T::default(); matrix_len];
+    let mut receivers = Vec::with_capacity(matrix_len);
     for i in 0..a.rows {
         for j in 0..b.cols {
-            for k in 0..a.cols {
-                data[i * b.cols + j] += a.data[i * a.cols + k] * b.data[k * b.cols + j];
-            }
+            let row = Vector::new(&a.data[i * a.cols..(i + 1) * a.cols]);
+            let col_data = b.data[j..]
+                .iter()
+                .step_by(b.cols)
+                .copied()
+                .collect::<Vec<_>>();
+            let col_ = Vector::new(col_data);
+            let idx = i * b.cols + j;
+            let input = MsgInput::new(idx, row, col_);
+            let (tx, rx) = oneshot::channel::<MsgOutput<T>>();
+            let msg = Msg::new(input, tx);
+            if let Err(e) = senders[idx % NUM_THREADS].send(msg) {
+                eprintln!("Error: {:?}", e);
+            };
+            receivers.push(rx);
+            // let ret=rx.recv()?;
+            // data[i * b.cols + j] += dot_product(row, col_)?;
         }
     }
+
+    for rx in receivers {
+        let ret = rx.recv()?;
+        data[ret.idx] = ret.value;
+    }
+
     Ok(Matrix::new(a.rows, b.cols, data))
 }
 
@@ -37,6 +107,12 @@ impl<T> Matrix<T> {
             rows,
             cols,
         }
+    }
+}
+
+impl<T> MsgInput<T> {
+    fn new(idx: usize, row: Vector<T>, col: Vector<T>) -> Self {
+        Self { idx, row, col }
     }
 }
 
